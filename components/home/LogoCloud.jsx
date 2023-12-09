15a1fab386a7eeb0d78dc7b2b586dcc73d1f9522
thecoderepublicdev <contact@thecoderepublic.dev>
2023-12-09 16:37:32 +0300

    Logo Cloud Added

--- components/home/LogoCloud.jsx
+++ components/home/LogoCloud.jsx
@@ -4,17 +4,19 @@ import Image from 'next/image';
 export default function LogoCloud() {
     console.log(require('@references/colored/tool-expo.svg'))
     return(
-        <div className="py-24 sm:py-32">
-            <div className="flex gap-4 justify-between align-center items-center content-center">
+        <div className="bg-black/5 py-6">
+            <div className="container mx-auto max-w-screen-2xl flex gap-4 justify-between align-center items-center content-center">
                 {References.map((reference, key) => reference.show && (
-                    <Image
-                        key={key}
-                        width={200}
-                        className="grayscale p-6 rounded-xl grid place-content-center place-items-center"
-                        height={80}
-                        src={reference.logo}
-                        alt={reference.brand}
-                    />
+                    <div className="w-full max-h-[120px] rounded-xl grid place-content-center place-items-center">
+                        <Image
+                            key={key}
+                            width={600}
+                            height={150}
+                            className="grayscale scale-120"
+                            src={reference.logo}
+                            alt={reference.brand}
+                        />
+                    </div>
                 ))}
             </div>
         </div>
--- data/_References.js
+++ data/_References.js
@@ -1,51 +1,51 @@
 const ReferencesData = [
     {   
         show: true,
-        logo: require('@references/colored/tool-expo.svg').default.src,
+        logo: require('@references/tool-expo.png').default.src,
         brand: "Tool Expo Kazakhstan",
-        url: "https://tool-expo.kz/?utm_source=thecoderepublic"
+        url: "https://tool-expo.kz/?utm_source=thecoderepublic",
     },
     {   
         show: true,
-        logo: require('@references/colored/greentech-expo.svg').default.src,
+        logo: require('@references/greentech-expo.png').default.src,
         brand: "GreenTech-Expo Kazakhstan",
-        url: "https://greentech-expo.kz/?utm_source=thecoderepublic"
+        url: "https://greentech-expo.kz/?utm_source=thecoderepublic",
     },
     {   
         show: true,
-        logo: require('@references/colored/windoor-expo.svg').default.src,
+        logo: require('@references/windoor-expo.png').default.src,
         brand: "Windoor-Expo Kazakhstan",
-        url: "https://windoor-expo.kz/?utm_source=thecoderepublic"
+        url: "https://windoor-expo.kz/?utm_source=thecoderepublic",
     },
     {   
         show: true,
-        logo: require('@references/colored/lift-expo-kazakhstan.png').default.src,
+        logo: require('@references/lift-expo.png').default.src,
         brand: "Lift Expo Kazakhstan",
-        url: "https://liftexpo.kz/?utm_source=thecoderepublic"
+        url: "https://liftexpo.kz/?utm_source=thecoderepublic",
     },
     {   
         show: true,
-        logo: require('@references/colored/intro.svg').default.src,
+        logo: require('@references/intro.png').default.src,
         brand: "Intro Fair Exhibition & Conference",
-        url: "https://introfair.com/?utm_source=thecoderepublic"
+        url: "https://introfair.com/?utm_source=thecoderepublic",
     },
     {   
         show: true,
-        logo: require('@references/black/tavsiye-psikolog.svg').default.src,
+        logo: require('@references/tavsiye-psikolog.png').default.src,
         brand: "Tavsiye Psikolog",
-        url: "https://tavsiyepsikolog.com.tr/?utm_source=thecoderepublic"
+        url: "https://tavsiyepsikolog.com.tr/?utm_source=thecoderepublic",
     },
     {   
         show: true,
-        logo: require('@references/colored/pasa-garden.png').default.src,
+        logo: require('@references/paşa-garden.png').default.src,
         brand: "Paşa Garden",
-        url: "https://pasagarden.com/?utm_source=thecoderepublic"
+        url: "https://pasagarden.com/?utm_source=thecoderepublic",
     },
     {   
         show: true,
-        logo: require('@references/black/bakery-expo.svg').default.src,
+        logo: require('@references/bakery-expo.png').default.src,
         brand: "Bakery Expo Kazakhstan",
-        url: "https://bakeryexpo.kz/?utm_source=thecoderepublic"
+        url: "https://bakeryexpo.kz/?utm_source=thecoderepublic",
     },
 ];
 
